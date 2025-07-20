a001efc9e68da5acfd44216168f9fd04fc7f0910
KaelidoscopeGG <carlmichael209@gmail.com>
2025-07-20 12:34:15 +0800

    Update main.cpp

--- arsClass/main.cpp
+++ arsClass/main.cpp
@@ -3,5 +3,46 @@
 using namespace std;
 
 int main() {
+  int choice;
+
+  do {
+      // main menu function
+      cin >> choice;
   
+      switch (choice) {
+      case 1:
+          // add available flights
+          break;
+      case 2:
+          // view available flights
+          break;
+      case 3:
+          // search flights
+          break;
+      case 4:
+          // save flight transactions
+          break;
+      case 5:
+          // book flight (remove available flights/add reservation)
+          break;
+      case 6:
+          // search reservation
+          break;
+      case 7:
+          // cancel reservation
+          break;
+      case 8:
+          // view current tickets
+          break;
+      case 9:
+          //save reservation transactions
+          break;
+      case 10:
+          cout << "Exiting program, thank you!";
+          return 0;
+          break;
+      default:
+          cout << "Please enter a number available in the list.\n\n";  
+      }
+  } while (true);
 }
